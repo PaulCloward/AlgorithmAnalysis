366f99df042179d65729ed92d0da616f5f522d07
PaulCloward <PaulClowardcs12@gmail.com>
2016-09-21 15:23:36 -0600

    Fermat commit

---
+++ Fermat/Fermat/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Fermat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Solve = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.K = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Solve
+            // 
+            this.Solve.Location = new System.Drawing.Point(164, 191);
+            this.Solve.Name = "Solve";
+            this.Solve.Size = new System.Drawing.Size(88, 41);
+            this.Solve.TabIndex = 0;
+            this.Solve.Text = "Solve";
+            this.Solve.UseVisualStyleBackColor = true;
+            this.Solve.Click += new System.EventHandler(this.Solve_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(83, 46);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(277, 20);
+            this.input.TabIndex = 1;
+            this.input.Text = "Input";
+            this.input.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // K
+            // 
+            this.K.Location = new System.Drawing.Point(83, 93);
+            this.K.Name = "K";
+            this.K.Size = new System.Drawing.Size(277, 20);
+            this.K.TabIndex = 2;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(83, 140);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(277, 20);
+            this.Output.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 261);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.K);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.Solve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Solve;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox K;
+        private System.Windows.Forms.TextBox Output;
+    }
+}
+
---
+++ Fermat/Fermat/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fermat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Solve_Click(object sender, EventArgs e) {
+            string inNum = input.Text;
+            string kNum = K.Text;
+
+            int x = Convert.ToInt32(inNum);
+            int k = Convert.ToInt32(kNum);
+            
+            Output.Text = primalityTest(x, k);
+            
+        }
+
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private string primalityTest(int x, int k) {
+            Random rand = new Random();
+            HashSet<int> valueSet = new HashSet<int>();
+            bool test = true;
+            
+            /*This for loop is to see if a^(n-1) == 1
+             * for k values 1 <= a < N */
+            for(int i = 0; i < k; i++) {
+
+                int randNum = rand.Next(1, x);
+                while (valueSet.Contains(randNum)) {//check for uniqueness
+                    randNum = rand.Next(1, k);
+                }
+
+                valueSet.Add(randNum);//add to valueSet once it's found unique
+
+                //This will recurse down
+                int result = modExp(randNum, (x - 1), x);//n^3
+
+               if(result != 1) {
+                    test = false;
+                    break;
+                }
+            }
+
+            if(test == false) {//Not Prime branch
+                return "No, definitely not Prime";
+            }
+            else {//prime branch which also shows probability of correctness
+                return "Yes it is Prime, with Probability " + Convert.ToString(1 - (1 / Math.Pow(2, k)));
+            }
+        }
+
+        private int modExp(int x, int y, int n) {
+            if(y == 0) {//constant
+                return 1;
+            }
+
+            /*This is where it will recurse down. There will
+            be n recursive steps*/
+            int z = modExp(x, (y / 2), n);
+            
+            if(y % 2 == 0) {//Checking if even. This is constant
+                return Convert.ToInt32(Math.Pow(z, 2)) % n;//n^2
+            }
+            else {//Will go to this branch if y is odd
+                return (x * Convert.ToInt32(Math.Pow(z, 2)))  % n;//n^2
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
